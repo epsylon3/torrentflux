65710bfd74cd169b57415b45ed301a10b30e1ee5
Epsylon3 <epsylon3@gmail.com>
2010-05-19 23:26:42 +0200

    fix: CONCAT() with sqlite for cookie domains

--- html/inc/classes/SimpleHTTP.php
+++ html/inc/classes/SimpleHTTP.php
@@ -347,7 +347,12 @@ class SimpleHTTP
 	    $this->getcmd .= (!empty($domain["query"])) ? "?" . $domain["query"] : "";
 
 		// Check to see if cookie required for this domain:
-		$sql = "SELECT c.data AS data FROM tf_cookies AS c LEFT JOIN tf_users AS u ON ( u.uid = c.uid ) WHERE u.user_id = ".$db->qstr($cfg["user"])." AND ".$db->qstr($domain['host'])." LIKE CONCAT('%',c.host)";
+		$sql = "SELECT c.data AS data FROM tf_cookies AS c LEFT JOIN tf_users AS u ON ( u.uid = c.uid ) WHERE u.user_id = ".$db->qstr($cfg["user"]);
+		if ($cfg["db_type"] != "sqlite")
+			$sql .= " AND ".$db->qstr($domain['host'])." LIKE CONCAT('%',c.host)";
+		else
+			$sql .= " AND ".$db->qstr($domain['host'])." LIKE PHP('sprintf','%%%s',c.host)";
+			
 		$this->cookie = $db->GetOne($sql);
 		if ($db->ErrorNo() != 0) dbError($sql);
 
