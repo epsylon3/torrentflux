d39d1ca8198e42085b062c34a2cf6ad358d6eb5b
Epsylon3 <epsylon3@gmail.com>
2010-05-20 14:47:26 +0200

    itoma: fix torrent name

--- html/inc/searchEngines/ItomaEngine.php
+++ html/inc/searchEngines/ItomaEngine.php
@@ -472,8 +472,8 @@ class fileItoma
 	function BuildOutput($bg, $searchURL = '')
 	{
 		$output = "<tr>\n";
-		$output .= "    <td width=16 bgcolor=\"".$bg."\"><a href=\"dispatcher.php?action=urlUpload&type=torrent&url=".$this->torrentFile."\"><img src=\"".getImagesPath()."download_owner.gif\" width=\"16\" height=\"16\" title=\"".$this->torrentName."\" border=0></a></td>\n";
-		$output .= "    <td bgcolor=\"".$bg."\"><a href=\"dispatcher.php?action=urlUpload&type=torrent&url=".$this->torrentFile."\" title=\"".$this->torrentName."\">".$this->torrentDisplayName."</a></td>\n";
+		$output .= "    <td width=16 bgcolor=\"".$bg."\"><a href=\"dispatcher.php?action=urlUpload&type=torrent&url=".urlencode($this->torrentFile)."\"><img src=\"".getImagesPath()."download_owner.gif\" width=\"16\" height=\"16\" title=\"".$this->torrentName."\" border=0></a></td>\n";
+		$output .= "    <td bgcolor=\"".$bg."\"><a href=\"dispatcher.php?action=urlUpload&type=torrent&url=".urlencode($this->torrentFile)."\" title=\"".$this->torrentName."\">".$this->torrentDisplayName."</a></td>\n";
 
 		if (strlen($this->MainCategory) > 1){        
 			$genre = $this->MainCategory;
