8e43e04edb3cab0cecfd3d91c17ee5b7af6a303d
Tentacule <BLM@.(none)>
2010-08-26 15:11:51 +0200

    Changing sort order
    - List folders first, then files
    - Case insensitive sorting

--- html/inc/iid/dir.php
+++ html/inc/iid/dir.php
@@ -375,6 +375,9 @@ else
 
 // read in entries
 $entrys = array();
+$entrysDirs = array();
+$entrysFiles = array();
+
 $handle = opendir($dirName);
 while (false !== ($entry = readdir($handle))) {
 	if (empty($dir)) { // parent dir
@@ -382,13 +385,24 @@ while (false !== ($entry = readdir($handle))) {
 			array_push($entrys, $entry);
 	} else { // sub-dir
 		if (hasPermission($dir, $cfg["user"], 'r')) {
-			if (isValidEntry($entry))
-				array_push($entrys, $entry);
+			if (isValidEntry($entry)) { //array_push($entrys, $entry);
+				if (is_dir($dirName.$entry)) {
+					array_push($entrysDirs, $entry);
+				   } else {
+					array_push($entrysFiles, $entry);
+				   }
+			}
+
 		}
 	}
 }
 closedir($handle);
-natsort($entrys);
+#natsort($entrys);
+#natcasesort($entrys);
+ 
+natcasesort($entrysDirs);
+natcasesort($entrysFiles); 
+$entrys = array_merge ($entrysDirs, $entrysFiles);
 
 // process entries and fill dir- + file-array
 $list = array();
