267545806a7b41e8c6dd118dbe7d2c1c31a124c7
Epsylon3 <epsylon3@gmail.com>
2010-11-30 22:50:22 +0100

    new command line tool to generate upgrade and install SQL scripts

--- sql/readme.txt
+++ sql/readme.txt
@@ -1,4 +1,7 @@
-to see updates needed to upgrade your database,
+to get lastest SQL update script,  use SQL update file generator command line tool :
 
-goto html/inc/install/
+ usage: ./sqlscript.php.sh mysql
+        ./sqlscript.php.sh sqlite
+        ./sqlscript.php.sh postgres
 
+to get the full install script : ./sqlscript.php.sh mysql install
---
+++ sql/sqlscript.php.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/php
+<?php
+/*
+ SQL update file generator - Command line tool
+ usage: ./sqlscript.php.sh mysql
+        ./sqlscript.php.sh sqlite
+        ./sqlscript.php.sh postgres
+*/
+
+$UPGRADE_FROM   = 'v1.0beta2';
+$UPGRADE_TO     = 'v1.0git';
+$DEFAULT_DBTYPE = 'mysql'; //'sqlite','mysql','postgres'
+
+$DBCONF = '../html/inc/config/config.db.php';
+
+global $argv;
+if (isset($argv[1]) )
+	$type = $argv[1];
+else
+	$type = $DEFAULT_DBTYPE;
+
+if (isset($argv[2]) && $argv[2]="install") {
+	$resfile='queries.install.php';
+	$UPGRADE_FROM = 'install';
+}
+else
+	$resfile='queries.upgrade.'.$UPGRADE_FROM.'.php';
+
+$queries = array();
+require('../html/inc/install/'.$resfile);
+global $queries;
+
+$res = "";
+foreach ($queries['create'][$type] as $databaseTypeName => $databaseQuery) {
+	echo $databaseQuery.";\n";
+	$res .= $databaseQuery.";\n";
+}
+echo "\n";
+$res .= "\n";
+foreach ($queries['data'][$type] as $databaseTypeName => $databaseQuery) {
+	echo $databaseQuery.";\n";
+	$res .= $databaseQuery.";\n";
+}
+
+file_put_contents("{$type}_{$UPGRADE_FROM}_{$UPGRADE_TO}.sql",$res);
